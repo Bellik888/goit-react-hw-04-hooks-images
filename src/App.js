fc60a48cd6478dd041a5d6ac532080213c4f21e8
Bellik888 <Bellik888@gmail.com>
2021-10-22 18:45:45 +0300

    add changes to App

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
-import { Component } from 'react';
+import { useState } from 'react';
 import './App.css';
 
 import { Searchbar } from './components/Searchbar/Searchbar';
 import { ImagesList } from './components/ImagesList/ImagesList';
 
-class App extends Component {
-  state = {
-    searchQuery: '',
+const App = () => {
+  const [searchQuery, setSearchQuery] = useState('');
+
+  const searchImages = value => {
+    setSearchQuery(value);
   };
 
-  searchImages = value => {
-    this.setState({ searchQuery: value });
-  };
+  return (
+    <div className="App">
+      <Searchbar onSubmit={searchImages} />
+      <ImagesList searchValue={searchQuery} />
+    </div>
+  );
+};
 
-  render() {
-    return (
-      <div className="App">
-        <Searchbar onSubmit={this.searchImages} />
-        <ImagesList searchValue={this.state.searchQuery} />
-      </div>
-    );
-  }
-}
+// class App extends Component {
+//   state = {
+//     searchQuery: '',
+//   };
+
+// searchImages = value => {
+//   this.setState({ searchQuery: value });
+// };
+
+//   render() {
+// return (
+//   <div className="App">
+//     <Searchbar onSubmit={this.searchImages} />
+//     <ImagesList searchValue={this.state.searchQuery} />
+//   </div>
+// );
+//   }
+// }
 
 export default App;
